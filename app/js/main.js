83a8f44e80e0cb545c8db3b52113dd0035a7e7b7
Pjery3 <mixxer1265755@gmail.com>
2021-02-20 09:10:09 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,36 @@ $(function () {
         arrows: false,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
       });
 
+      $('.header__burger').on('click', function(){
+          $('.header__nav').slideToggle();
+      })
 
 });
 
